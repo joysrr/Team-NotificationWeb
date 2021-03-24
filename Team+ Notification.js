06f680bcb6b22826d295fa4977a7be85a3c2ffe4
LuLuLaLaHaHa <swdax545@gmail.com>
2021-03-24 13:45:32 +0800

    Update Team+ Notification.js

--- Team+ Notification.js
+++ Team+ Notification.js
@@ -1,41 +1,55 @@
 // ==UserScript==
 // @name         Team+ Notification
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      0.1
 // @description  Show Team+ Notification by browser
 // @author       joysrr
-// @match        *
+// @match        http://tp.cht-pt.com.tw/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
     if (('Notification' in window)) {
-        let currcount = 0;
+        let unreadcount = 0, title = '';
         setInterval(()=>{
-            const notice = document.querySelectorAll('.noticeBadge')
-            for(let i = 0; i < notice.length;i++){
-                currcount = parseInt(notice[i].innerText);
-                if(currcount>0){
-                    showNotify(currcount);
-                    break;
+            const chatView = document.getElementById('divPersonalLogSideBar')
+            if(chatView){
+                //chat page
+                const unreadList = chatView.querySelectorAll('.unreadCount');
+                unreadList.forEach(unread=>{
+                    unreadcount = unread.innerText;
+                    title = unread.parentElement.querySelector('.chatName').title;
+                    showNotify(unreadcount, title);
+                })
+            }else{
+                // other page
+                const notice = document.querySelectorAll('.noticeBadge')
+                for(let i = 0; i < notice.length;i++){
+                    unreadcount = parseInt(notice[i].innerText);
+                    if(unreadcount>0){
+                        showNotify(unreadcount);
+                        break;
+                    }
                 }
             }
         }, 10000)
     }
 
-    const notifyConfig = {
-        body: '您有新的訊息，請回復!', // 設定內容
-        icon: '../Images/chatsbg.png', // 設定 icon
-        tag:'Team+Notification',// 標籤
-        renotify: true, // 重新通知
-    };
-
-    function showNotify(num = '*'){
+    function showNotify(unread = '*', tag = 'default'){
+        // 使用者同意授權
         Notification.requestPermission(function(permission) {
             if (permission === 'granted') {
-                // 使用者同意授權
-                var notification = new Notification(`Team+ Message[${num.toString()}]`, notifyConfig); // 建立通知
+                // 建立通知
+                var notification = new Notification(`Team+[${tag=='default'?unread:tag}]`,{
+                    body: `您有${unread}則新的訊息，請回覆!`, // 設定內容
+                    icon: '../Images/chatsbg.png', // 設定 icon
+                    tag:tag, // 標籤
+                    renotify: true, // 重新通知
+                }).onclick = function(e) { // 點擊
+                    e.preventDefault();
+                    window.open('http://tp.cht-pt.com.tw/EIM/Chat/ChatMain.aspx'); // 打開Team+訊息視窗
+                };
             }
         });
     }
